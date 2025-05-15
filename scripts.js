eacc6160d72c5f271c96ebfc4746f3e998f59d9b
Renan dos Santos <40153166+renandossantos@users.noreply.github.com>
2025-05-15 04:02:56 +0000

    Add new file `resultados-maio.js`

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-import showResults from "./database.js";
+import showResults from "./resultados-maio.js";
 
 const resultados = showResults(); // resultados importados do Database
 const mostrarNaTela = document.querySelector("#output"); // Cria Section com os dados na tela
